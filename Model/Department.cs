9cfa73687697df40fd23a67da21dd2b74be24086
shivang104 <67458972+shivang104@users.noreply.github.com>
2020-07-03 22:03:40 +0530

    Add files via upload

---
+++ Model/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Department
+    {   
+        public string Depid { get; set; }
+        public string Depname { get; set; }
+        public string Depmanager { get; set; }
+    }
+}
---
+++ Model/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Model
+{
+    public class Employee
+    {
+        public string empid { get; set; }
+        public string name { get; set; }
+        public string gender { get; set; }
+        public string department { get; set; }
+
+    }
+}
